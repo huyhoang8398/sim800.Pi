55df824bf0d734325ea7cf9672224299cada7011
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-15 16:58:12 +0700

    updating,.....

--- bin/changeTimeCron.sh
+++ bin/changeTimeCron.sh
@@ -4,11 +4,44 @@
 ### 'change *2/ * * * *' ######
 
 # first delete the single quote from message 
-sed "s/['\"]//g" ChangeTime.txt | tee nChangeTime.txt
+sed "s/['\"]//g" changeTime.txt > nChangeTime.txt
 ### then delete the first word "change"
-cut -d" " -f2- nChangeTime.txt | tee nChangeTime.txt
+echo $(cut -d" " -f2- nChangeTime.txt) > nChangeTime.txt
+#after that, we have something like "*/2 * * * *" in nChangeTime.txt
+###### Each entry in a crontab file consists of six fields, specifying in the following order: ###
+##   minute(s) hour(s) day(s) month(s) weekday(s) command(s) ##
+# now we assign value from nChangeTime.txt ##
 
-crontab -l > cron.txt 
-cat cron.txt | grep scan-lib | head -n1 
+### minutes
+minut=$(cat nChangeTime.txt | head -n1 | awk '{print $1;}')
+## hour
+hour=$(cat nChangeTime.txt | head -n1 | awk '{print $2;}')
+# days 
+days=$(cat nChangeTime.txt | head -n1 | awk '{print $3;}')
+# month 
+month=$(cat nChangeTime.txt | head -n1 | awk '{print $4;}')
+# weekdays
+weekday=$(cat nChangeTime.txt | head -n1 | awk '{print $5;}')
+
+### we check the cronjob, then export it to the 
+crontab -l > cron.txt  #### Check current scheduled cron:
+
+echo $(cat cron.txt | grep scan-lib | head -n1) > cron.txt # grep scanner job
+
+## same thing with nChangeTime.txt#
+minutOrig=$(cat cron.txt | awk '{print $1}')
+hourOrig=$(cat cron.txt | awk '{print $2}')
+daysOrig=$(cat cron.txt | awk '{print $3}')
+monthOrig=$(cat cron.txt | awk '{print $4}')
+weakdayOrig=$(cat cron.txt | awk '{print $5}')
+
+###### Now we sawp minut to minut Original 
+sed -i -e "s|$minutOrig|$minut|g" cron.txt 
+sed -i -e "s|$hourOrig|$hour|g" cron.txt 
+sed -i -e "s|$daysOrig|$days|g" cron.txt
+sed -i -e "s|$monthOrig|$month|g" cron.txt 
+sed -i -e "s|$weakdayOrig|$weekday|g" cron.txt 
+
+
 
 
