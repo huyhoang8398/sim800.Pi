7d596fece1dad15304d26a28ff5742a0285384c2
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-21 20:50:50 +0700

    script to change DPI scanner

---
+++ bin/changeDPI.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# first delete the single quote from message 
+sed "s/['\"]//g" changeDPI.txt > nChangeDPI.txt
+### then delete the first word "change"
+echo "$(cut -d" " -f2- nChangeDPI.txt)" > nChangeDPI.txt
+DPI=$(cat nChangeDPI.txt)
+infoDPI=$(crontab -l > fileTmp.txt; < fileTmp.txt grep java | awk '{print $(NF-2)}')
+crontab -l > file.txt; < file.txt sed -e "s|$infoDPI|$DPI|" | crontab -
+#sed -e "s|$monthOrig|$month|4" cron.txt
