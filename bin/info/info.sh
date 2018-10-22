cf2c7d8f2a534bdc0da4c81ca91f67fe522e4805
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-10-22 21:09:41 +0700

    updated info script

--- bin/info/info.sh
+++ bin/info/info.sh
@@ -6,15 +6,17 @@ str4="DPI:" ## your DPI
 
 #Linux command line 
 avaiMemG=$(df -h / | tail -1 | awk '{print $4}')
-infoPng=$(find /home/pi/scann/pictures | grep .png | wc -l)
-infoJpg=$(find /home/pi/scann/pictures | grep .jpg | wc -l)
+#infoPng=$(find /home/pi/scann/pictures | grep .png | wc -l)
+#infoJpg=$(find /home/pi/scann/pictures | grep .jpg | wc -l)
 infoDPI=$(crontab -l > fileTmp.txt; < fileTmp.txt grep java | awk '{print $(NF-2)}')
 
-avaiMemK=$(df -k / | tail -1 | awk '{print $4}')
+declare -i avaiMemK=$(df -k / | tail -1 | awk '{print $4}')
+# declare -i numIMG=0
 ### image size
 ## 1GiB = 1024 MB = 1048576 KB
 # df -h will output as gigabyte if posible, but we need KB and MB 
-
+# using kb as default parameter
+declare -i z=6144
 
 
 function GET_MEM()
@@ -35,20 +37,23 @@ function GetDPI()
 {
 	echo -e "$str4 $infoDPI\n"
 }
-function imgLeft()
-{
 
-}
+if [ $infoDPI = "300" ]
+then
+	numIMG=$((avaiMemK / z))
+	echo "$numIMG" >> /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt
+fi
 
+# imgLeft $infoDPI $avaiMemK $z $numIMG
 
 # Write date, time > overwrite all files data
-date > /home/pi/scann/log/dailyLog.txt
+date > /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt
 
 # Echo output to textfile without overwrite date time 
-echo $(GET_JPG) >> /home/pi/scann/log/dailyLog.txt
-echo $(GET_PNG) >> /home/pi/scann/log/dailyLog.txt 
-echo $(GET_MEM) >> /home/pi/scann/log/dailyLog.txt 
-echo $(GetDPI) >> /home/pi/scann/log/dailyLog.txt 
+echo $(GET_JPG) >> /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt
+echo $(GET_PNG) >> /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt 
+echo $(GET_MEM) >> /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt 
+echo $(GetDPI) >> /Users/huyhoang8398/sim800.Pi/scann/log/dailyLog.txt 
 
 
 
