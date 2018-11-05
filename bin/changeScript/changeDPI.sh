f0dbca44937dc3773b569e6c4829f6d4d9b0481b
huyhoang8398 <huyhoang8398@huyhoang8398s-MacBook-Pro.local>
2018-11-05 10:05:19 +0700

    Updated

--- bin/changeScript/changeDPI.sh
+++ bin/changeScript/changeDPI.sh
@@ -5,7 +5,7 @@ sed "s/['\"]//g" changeDPI.txt > nChangeDPI.txt
 echo "$(cut -d" " -f2- nChangeDPI.txt)" > nChangeDPI.txt
 DPI=$(cat nChangeDPI.txt)
 infoDPI=$(crontab -l > fileTmp.txt; < fileTmp.txt grep java | awk '{print $(NF-2)}')
-echo "$DPI"
-echo "$infoDPI"
+#echo "$DPI"
+#echo "$infoDPI"
 < fileTmp.txt sed -e "s|$infoDPI|$DPI|" | crontab -
-#sed -e "s|$monthOrig|$month|4" cron.txt
+
